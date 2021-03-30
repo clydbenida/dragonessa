a99a342778284828fa810b969f3e25abf254a780
Benedict Benida <clyd.benidz@gmail.com>
2021-03-30 13:47:45 +0800

    added ckeditor for richtextfield purposes

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}));
 
+function removeTags(str) {
+   if ((str===null) || (str===''))
+   return false;
+   else
+   str = str.toString();
+   return str.replace( /(<([^>]+)>)/ig, '');
+}
+
 app.get("/", (req, res) => {
    res.render("index");
 });
@@ -32,30 +40,42 @@ app.get("/admin/dashboard", (req, res) => {
    res.render("./admin/dashboard");
 })
 
-app.get("/admin/products", (req, res) => {
-   Product.find({})
-      .then(p => {
-         const products = [...p]
-         res.render("./admin/show-products", { products });
-      })
-      .catch(err => {
-         console.log(err);
-         res.render("./admin/show-products")
-      })
-   
+app.get("/admin/products", async (req, res) => {
+   try {
+      const products = await Product.find({})
+
+      for (let p of products) {
+         const newDesc = removeTags(p.desc)
+         p.desc = newDesc.slice(0, 28).concat("...")
+      }
+      res.render("./admin/show-products", { products });
+   } catch (err) {
+      console.log(err);
+      res.render("./admin/show-products")
+   }
 })
 
 app.get("/admin/products/new", (req, res) => {
    res.render("./admin/new-product");
 })
 
-app.post("/admin/products", (req, res) => {
+app.post("/admin/products", async (req, res) => {
    const { name, qty, desc, price } = req.body;
-   const newProduct = new Product({
-      name, qty, desc, price
-   }).save()
-      .then(() => res.redirect("/admin/products"))
-      .catch(err => console.log(err))
+   try {
+      const newProduct = await new Product({
+         name, qty, desc, price
+      }).save()
+      res.redirect("/admin/products")
+   } catch(err) {
+      console.log(err)
+   }
+   
+   // const { name, qty, desc, price } = req.body;
+   // const newProduct = await new Product({
+   //    name, qty, desc, price
+   // }).save()
+   //    .then(() => res.redirect("/admin/products"))
+   //    .catch(err => console.log(err))
 })
 
 app.listen(3000, () => console.log('Server is running'));
